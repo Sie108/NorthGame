b9e8c81a0834e3832b617d28c1a98e707e7ccf49
Sierra Weber <10889154>
2021-08-26 18:00:59 -0600

    Code pt 1

---
+++ NORTHGAME/Assets/CamSwitch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CamSwitch : MonoBehaviour {
+
+    public Camera FpsCam;
+    public Camera TpsCam;
+    bool fpsCam = true;
+
+    // Use this for initialization
+    void Start () {
+        FpsCam.enabled = fpsCam;
+        TpsCam.enabled = !fpsCam;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            fpsCam = !fpsCam;
+            FpsCam.enabled = fpsCam;
+            TpsCam.enabled = !fpsCam;
+        }
+    }
+        
+        //end o class
+    }
